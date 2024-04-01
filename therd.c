e55666c821ab8d1dd0b4355bf06826ad9c7c79b7
Mike Klein <mtklein@gmail.com>
2024-04-01 11:45:11 -0500

    names

--- therd.c
+++ therd.c
@@ -83,109 +83,110 @@ struct Program* compile(struct Builder *b) {
 
 #define defn(name) static void name(struct Inst const *ip, int i, int n, void* ptr[], \
                                     F v0, F v1, F v2, F v3, F v4, F v5, F v6, F v7)
-defn(mul2) { v0 *= v1; next; }
-defn(mul3) { v1 *= v2; next; }
-defn(mul4) { v2 *= v3; next; }
-defn(mul5) { v3 *= v4; next; }
-defn(mul6) { v4 *= v5; next; }
-defn(mul7) { v5 *= v6; next; }
-defn(mul8) { v6 *= v7; next; }
+defn(mul_2) { v0 *= v1; next; }
+defn(mul_3) { v1 *= v2; next; }
+defn(mul_4) { v2 *= v3; next; }
+defn(mul_5) { v3 *= v4; next; }
+defn(mul_6) { v4 *= v5; next; }
+defn(mul_7) { v5 *= v6; next; }
+defn(mul_8) { v6 *= v7; next; }
 void mul(struct Builder *b) {
     assert(b->depth >= 2);
-    static Fn *fn[9] = {0,0,mul2,mul3,mul4,mul5,mul6,mul7,mul8};
+    static Fn *fn[9] = {0,0,mul_2,mul_3,mul_4,mul_5,mul_6,mul_7,mul_8};
     struct Inst inst = { .fn=fn[b->depth] };
     push(b,inst,inst);
     b->depth -= 1;
 }
 
-defn(add2) { v0 += v1; next; }
-defn(add3) { v1 += v2; next; }
-defn(add4) { v2 += v3; next; }
-defn(add5) { v3 += v4; next; }
-defn(add6) { v4 += v5; next; }
-defn(add7) { v5 += v6; next; }
-defn(add8) { v6 += v7; next; }
+defn(add_2) { v0 += v1; next; }
+defn(add_3) { v1 += v2; next; }
+defn(add_4) { v2 += v3; next; }
+defn(add_5) { v3 += v4; next; }
+defn(add_6) { v4 += v5; next; }
+defn(add_7) { v5 += v6; next; }
+defn(add_8) { v6 += v7; next; }
 
-defn(mad3) { v0 += v1*v2; next; }
+defn(mad_3) { v0 += v1*v2; next; }
 
 void add(struct Builder *b) {
     assert(b->depth >= 2);
-    if (b->body[b->insts-1].fn == mul3) {
-        b->body[b->insts-1].fn =  mad3;
-        b->tail[b->insts-1].fn =  mad3;
+    if (b->body[b->insts-1].fn == mul_3) {
+        b->body[b->insts-1].fn =  mad_3;
+        b->tail[b->insts-1].fn =  mad_3;
         b->depth -= 1;
         return;
     }
-    static Fn *fn[9] = {0,0,add2,add3,add4,add5,add6,add7,add8};
+    static Fn *fn[9] = {0,0,add_2,add_3,add_4,add_5,add_6,add_7,add_8};
     struct Inst inst = { .fn=fn[b->depth] };
     push(b,inst,inst);
     b->depth -= 1;
 }
 
-defn(bstore1) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v0, sizeof v0   ); next; }
-defn(bstore2) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v1, sizeof v1   ); next; }
-defn(bstore3) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v2, sizeof v2   ); next; }
-defn(bstore4) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v3, sizeof v3   ); next; }
-defn(bstore5) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v4, sizeof v4   ); next; }
-defn(bstore6) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v5, sizeof v5   ); next; }
-defn(bstore7) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v6, sizeof v6   ); next; }
-defn(bstore8) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v7, sizeof v7   ); next; }
+defn(storeK_1) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v0, sizeof v0   ); next; }
+defn(storeK_2) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v1, sizeof v1   ); next; }
+defn(storeK_3) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v2, sizeof v2   ); next; }
+defn(storeK_4) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v3, sizeof v3   ); next; }
+defn(storeK_5) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v4, sizeof v4   ); next; }
+defn(storeK_6) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v5, sizeof v5   ); next; }
+defn(storeK_7) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v6, sizeof v6   ); next; }
+defn(storeK_8) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v7, sizeof v7   ); next; }
 
-defn(tstore1) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v0, sizeof v0[0]); next; }
-defn(tstore2) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v1, sizeof v1[0]); next; }
-defn(tstore3) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v2, sizeof v2[0]); next; }
-defn(tstore4) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v3, sizeof v3[0]); next; }
-defn(tstore5) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v4, sizeof v4[0]); next; }
-defn(tstore6) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v5, sizeof v5[0]); next; }
-defn(tstore7) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v6, sizeof v6[0]); next; }
-defn(tstore8) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v7, sizeof v7[0]); next; }
+defn(store1_1) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v0, sizeof v0[0]); next; }
+defn(store1_2) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v1, sizeof v1[0]); next; }
+defn(store1_3) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v2, sizeof v2[0]); next; }
+defn(store1_4) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v3, sizeof v3[0]); next; }
+defn(store1_5) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v4, sizeof v4[0]); next; }
+defn(store1_6) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v5, sizeof v5[0]); next; }
+defn(store1_7) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v6, sizeof v6[0]); next; }
+defn(store1_8) { float *dst = ptr[ip->ix]; memcpy(dst+i, &v7, sizeof v7[0]); next; }
 
 void store(struct Builder *b, int ix) {
     assert(b->depth >= 1);
-    static Fn *body[9] = {0,bstore1,bstore2,bstore3,bstore4,bstore5,bstore6,bstore7,bstore8},
-              *tail[9] = {0,tstore1,tstore2,tstore3,tstore4,tstore5,tstore6,tstore7,tstore8};
+    static Fn
+        *body[9] = {0,storeK_1,storeK_2,storeK_3,storeK_4,storeK_5,storeK_6,storeK_7,storeK_8},
+        *tail[9] = {0,store1_1,store1_2,store1_3,store1_4,store1_5,store1_6,store1_7,store1_8};
     push(b, (struct Inst){.fn=body[b->depth], .ix=ix}
           , (struct Inst){.fn=tail[b->depth], .ix=ix});
 }
 
-defn(bload0) { float const *src = ptr[ip->ix]; memcpy(&v0, src+i, sizeof v0   ); next; }
-defn(bload1) { float const *src = ptr[ip->ix]; memcpy(&v1, src+i, sizeof v1   ); next; }
-defn(bload2) { float const *src = ptr[ip->ix]; memcpy(&v2, src+i, sizeof v2   ); next; }
-defn(bload3) { float const *src = ptr[ip->ix]; memcpy(&v3, src+i, sizeof v3   ); next; }
-defn(bload4) { float const *src = ptr[ip->ix]; memcpy(&v4, src+i, sizeof v4   ); next; }
-defn(bload5) { float const *src = ptr[ip->ix]; memcpy(&v5, src+i, sizeof v5   ); next; }
-defn(bload6) { float const *src = ptr[ip->ix]; memcpy(&v6, src+i, sizeof v6   ); next; }
-defn(bload7) { float const *src = ptr[ip->ix]; memcpy(&v7, src+i, sizeof v7   ); next; }
+defn(loadK_0) { float const *src = ptr[ip->ix]; memcpy(&v0, src+i, sizeof v0   ); next; }
+defn(loadK_1) { float const *src = ptr[ip->ix]; memcpy(&v1, src+i, sizeof v1   ); next; }
+defn(loadK_2) { float const *src = ptr[ip->ix]; memcpy(&v2, src+i, sizeof v2   ); next; }
+defn(loadK_3) { float const *src = ptr[ip->ix]; memcpy(&v3, src+i, sizeof v3   ); next; }
+defn(loadK_4) { float const *src = ptr[ip->ix]; memcpy(&v4, src+i, sizeof v4   ); next; }
+defn(loadK_5) { float const *src = ptr[ip->ix]; memcpy(&v5, src+i, sizeof v5   ); next; }
+defn(loadK_6) { float const *src = ptr[ip->ix]; memcpy(&v6, src+i, sizeof v6   ); next; }
+defn(loadK_7) { float const *src = ptr[ip->ix]; memcpy(&v7, src+i, sizeof v7   ); next; }
 
-defn(tload0) { float const *src = ptr[ip->ix]; memcpy(&v0, src+i, sizeof v0[0]); next; }
-defn(tload1) { float const *src = ptr[ip->ix]; memcpy(&v1, src+i, sizeof v1[0]); next; }
-defn(tload2) { float const *src = ptr[ip->ix]; memcpy(&v2, src+i, sizeof v2[0]); next; }
-defn(tload3) { float const *src = ptr[ip->ix]; memcpy(&v3, src+i, sizeof v3[0]); next; }
-defn(tload4) { float const *src = ptr[ip->ix]; memcpy(&v4, src+i, sizeof v4[0]); next; }
-defn(tload5) { float const *src = ptr[ip->ix]; memcpy(&v5, src+i, sizeof v5[0]); next; }
-defn(tload6) { float const *src = ptr[ip->ix]; memcpy(&v6, src+i, sizeof v6[0]); next; }
-defn(tload7) { float const *src = ptr[ip->ix]; memcpy(&v7, src+i, sizeof v7[0]); next; }
+defn(load1_0) { float const *src = ptr[ip->ix]; memcpy(&v0, src+i, sizeof v0[0]); next; }
+defn(load1_1) { float const *src = ptr[ip->ix]; memcpy(&v1, src+i, sizeof v1[0]); next; }
+defn(load1_2) { float const *src = ptr[ip->ix]; memcpy(&v2, src+i, sizeof v2[0]); next; }
+defn(load1_3) { float const *src = ptr[ip->ix]; memcpy(&v3, src+i, sizeof v3[0]); next; }
+defn(load1_4) { float const *src = ptr[ip->ix]; memcpy(&v4, src+i, sizeof v4[0]); next; }
+defn(load1_5) { float const *src = ptr[ip->ix]; memcpy(&v5, src+i, sizeof v5[0]); next; }
+defn(load1_6) { float const *src = ptr[ip->ix]; memcpy(&v6, src+i, sizeof v6[0]); next; }
+defn(load1_7) { float const *src = ptr[ip->ix]; memcpy(&v7, src+i, sizeof v7[0]); next; }
 
 void load(struct Builder *b, int ix) {
     assert(b->depth < 8);
-    static Fn *body[9] = {bload0,bload1,bload2,bload3,bload4,bload5,bload6,bload7,0},
-              *tail[9] = {tload0,tload1,tload2,tload3,tload4,tload5,tload6,tload7,0};
+    static Fn *body[9] = {loadK_0,loadK_1,loadK_2,loadK_3,loadK_4,loadK_5,loadK_6,loadK_7,0},
+              *tail[9] = {load1_0,load1_1,load1_2,load1_3,load1_4,load1_5,load1_6,load1_7,0};
     push(b, (struct Inst){.fn=body[b->depth], .ix=ix}
           , (struct Inst){.fn=tail[b->depth], .ix=ix});
     b->depth += 1;
 }
 
-defn(splat0) { v0 = ((F){0} + 1) * ip->imm; next; }
-defn(splat1) { v1 = ((F){0} + 1) * ip->imm; next; }
-defn(splat2) { v2 = ((F){0} + 1) * ip->imm; next; }
-defn(splat3) { v3 = ((F){0} + 1) * ip->imm; next; }
-defn(splat4) { v4 = ((F){0} + 1) * ip->imm; next; }
-defn(splat5) { v5 = ((F){0} + 1) * ip->imm; next; }
-defn(splat6) { v6 = ((F){0} + 1) * ip->imm; next; }
-defn(splat7) { v7 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_0) { v0 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_1) { v1 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_2) { v2 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_3) { v3 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_4) { v4 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_5) { v5 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_6) { v6 = ((F){0} + 1) * ip->imm; next; }
+defn(splat_7) { v7 = ((F){0} + 1) * ip->imm; next; }
 void splat(struct Builder *b, float imm) {
     assert(b->depth < 8);
-    static Fn *fn[9] = {splat0,splat1,splat2,splat3,splat4,splat5,splat6,splat7,0};
+    static Fn *fn[9] = {splat_0,splat_1,splat_2,splat_3,splat_4,splat_5,splat_6,splat_7,0};
     struct Inst inst = {.fn=fn[b->depth], .imm=imm};
     push(b,inst,inst);
     b->depth += 1;
