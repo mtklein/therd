4778afc7f80cce2dab2e7433343c224d362bd710
Mike Klein <mtklein@gmail.com>
2024-04-23 10:06:00 -0500

    bake in benchmarking

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define len(x) (int)( sizeof(x) / sizeof(*x) )
 #define want(x) if (!(x)) dprintf(2, "%s:%d want(%s)\n", __FILE__, __LINE__, #x), __builtin_trap()
@@ -12,7 +13,7 @@ static _Bool equiv(float x, float y) {
         || (x != x && y != y);
 }
 
-static void test_3xp2(int const loops) {
+static double test_3xp2(int const loops) {
     float const src[] = {1,2,3,4,5,6,7,8,9,10,11};
     for (int n = 0; n <= len(src) ; n++) {
         float dst[len(src)] = {0};
@@ -30,9 +31,10 @@ static void test_3xp2(int const loops) {
             want(equiv(dst[i], i < n ? 3*src[i] + 2 : 0));
         }
     }
+    return len(src);
 }
 
-static void test_demo(int const loops) {
+static double test_demo(int const loops) {
     int const w = 319,
               h = 240;
     struct { float r,g,b; } * const rgb = calloc((size_t)(w*h), sizeof *rgb);
@@ -55,9 +57,10 @@ static void test_demo(int const loops) {
         stbi_write_hdr("/dev/stdout", w,h,3, &rgb->r);
     }
     free(rgb);
+    return w*h;
 }
 
-static void test_naive(int const loops) {
+static double test_naive(int const loops) {
     int const w = 319,
               h = 240;
     struct { float r,g,b; } * const rgb = calloc((size_t)(w*h), sizeof *rgb);
@@ -75,13 +78,42 @@ static void test_naive(int const loops) {
         stbi_write_hdr("/dev/stdout", w,h,3, &rgb->r);
     }
     free(rgb);
+    return w*h;
 }
 
-#define test(fn) if (loops == 1 || 0 == strcmp(bench, #fn)) test_##fn(loops)
-int main(int argc, char* argv[]) {
-    char const *bench = argc > 1 ?      argv[1]  : "demo";
-    int  const  loops = argc > 2 ? atoi(argv[2]) : 1;
+static double time_ns(double (*fn)(int), int loops) {
+    struct timespec b,e;
+    want(clock_gettime(CLOCK_MONOTONIC, &b) == 0);
+    fn(loops);
+    want(clock_gettime(CLOCK_MONOTONIC, &e) == 0);
+    return (double)(e.tv_sec  - b.tv_sec ) * 1e9
+         + (double)(e.tv_nsec - b.tv_nsec);
+}
 
+static void bench(char const *label, double (*fn)(int)) {
+    double const units = fn(2);
+
+    int loops = 1;
+    for (double elapsed = 0; elapsed < 5e6; ) {
+        loops *= 2;
+        elapsed = time_ns(fn, loops);
+    }
+
+    double min = 1.0/0.0;
+    for (int i = 0; i < 20; i++) {
+        double const sample = time_ns(fn, loops) / loops;
+        min = min < sample ? min : sample;
+    }
+
+    printf("%s\t%10.1f\t(%.2f)\n", label,min, min/units);
+}
+
+#define test(fn) \
+    if (0 == strcmp(focus, "") || 0 == strcmp(focus, #fn)) test_##fn(1); \
+    if (0 == strcmp(focus, "bench")) bench(#fn, test_##fn)
+
+int main(int argc, char* argv[]) {
+    char const *focus = argc > 1 ? argv[1] : "";
     test(3xp2);
     test(demo);
     test(naive);
